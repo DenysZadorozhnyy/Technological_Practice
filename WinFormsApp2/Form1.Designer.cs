630a2dd020e532fb3504c8bb925cf55af117cbb8
denys <denysz2004@gmail.com>
2024-07-17 17:11:02 +0200

    Add project files.

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            Ukr = new Label();
+            Math = new Label();
+            Hystory = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(180, 28);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(44, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(180, 57);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(44, 23);
+            textBox2.TabIndex = 0;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(180, 86);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(44, 23);
+            textBox3.TabIndex = 0;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // Ukr
+            // 
+            Ukr.AccessibleRole = AccessibleRole.Text;
+            Ukr.AutoSize = true;
+            Ukr.Font = new Font("Segoe UI", 14F);
+            Ukr.ForeColor = SystemColors.ActiveCaptionText;
+            Ukr.ImageKey = "(none)";
+            Ukr.Location = new Point(12, 23);
+            Ukr.Name = "Ukr";
+            Ukr.Size = new Size(156, 25);
+            Ukr.TabIndex = 1;
+            Ukr.Text = "Українська мова";
+            Ukr.Click += label1_Click;
+            // 
+            // Math
+            // 
+            Math.AccessibleRole = AccessibleRole.Text;
+            Math.AutoSize = true;
+            Math.Font = new Font("Segoe UI", 14F);
+            Math.ForeColor = SystemColors.ActiveCaptionText;
+            Math.ImageKey = "(none)";
+            Math.Location = new Point(28, 52);
+            Math.Name = "Math";
+            Math.Size = new Size(118, 25);
+            Math.TabIndex = 1;
+            Math.Text = "Математика";
+            Math.Click += label1_Click;
+            // 
+            // Hystory
+            // 
+            Hystory.AccessibleRole = AccessibleRole.Text;
+            Hystory.AutoSize = true;
+            Hystory.Font = new Font("Segoe UI", 14F);
+            Hystory.ForeColor = SystemColors.ActiveCaptionText;
+            Hystory.ImageKey = "(none)";
+            Hystory.Location = new Point(18, 77);
+            Hystory.Name = "Hystory";
+            Hystory.Size = new Size(144, 25);
+            Hystory.TabIndex = 1;
+            Hystory.Text = "Історія України";
+            Hystory.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.System;
+            button1.Font = new Font("Segoe UI", 10F);
+            button1.ForeColor = SystemColors.Highlight;
+            button1.Location = new Point(205, 178);
+            button1.Name = "button1";
+            button1.Size = new Size(95, 39);
+            button1.TabIndex = 2;
+            button1.Text = "Обрахувати";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(319, 226);
+            Controls.Add(button1);
+            Controls.Add(Hystory);
+            Controls.Add(Math);
+            Controls.Add(Ukr);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            ForeColor = SystemColors.AppWorkspace;
+            Name = "Form1";
+            Text = "Program";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label Ukr;
+        private Label Math;
+        private Label Hystory;
+        private Button button1;
+    }
+}
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,511 @@
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterParent;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string Ukr = textBox1.Text;
+            string mark_Ukr = Ukr;
+            switch (Ukr)
+            {
+                case "7":
+                    mark_Ukr = "100 балів";
+                    break;
+
+                case "8":
+                    mark_Ukr = "105 балів";
+                    break;
+
+                case "9":
+                    mark_Ukr = "110 балів";
+                    break;
+
+                case "10":
+                    mark_Ukr = "115 балів";
+                    break;
+
+                case "11":
+                    mark_Ukr = "120 балів";
+                    break;
+
+                case " 12":
+                    mark_Ukr = "125 балів";
+                    break;
+
+                case "13":
+                    mark_Ukr =  "131 бал";
+                    break;
+
+                case "14":
+                    mark_Ukr = "134 бали";
+                    break;
+
+                case "15":
+                    mark_Ukr = "136 балів";
+                    break;
+
+                case "16":
+                    mark_Ukr = "138 балів";
+                    break;
+
+                case "17":
+                    mark_Ukr = "140 балів";
+                    break;
+
+                case "18":
+                    mark_Ukr = "142 бали";
+                    break;
+
+                case "19":
+                    mark_Ukr = "143 бали";
+                    break;
+
+                case "20":
+                    mark_Ukr = "144 бали";
+                    break;
+
+                case "21":
+                    mark_Ukr = "145 балів";
+                    break;
+
+                case "22":
+                    mark_Ukr = "146 балів";
+                    break;
+
+                case " 23":
+                    mark_Ukr = "148 балів";
+                    break;
+
+                case "24":
+                    mark_Ukr = "149 балів";
+                    break;
+
+                case "25":
+                    mark_Ukr = "150 балів";
+                    break;
+
+                case "26":
+                    mark_Ukr = "152 бали ";
+                    break;
+
+                case "27":
+                    mark_Ukr = "154 бали";
+                    break;
+
+                case "28":
+                    mark_Ukr = "156 балів";
+                    break;
+
+                case "29":
+                    mark_Ukr = "157 балів";
+                    break;
+
+                case "30":
+                    mark_Ukr = "159 балів";
+                    break;
+
+                case "31":
+                    mark_Ukr = "160 балів";
+                    break;
+
+                case "32":
+                    mark_Ukr = "162 бали";
+                    break;
+
+                case "33":
+                    mark_Ukr = "163 бали";
+                    break;
+
+                case "34":
+                    mark_Ukr = "165 балів";
+                    break;
+
+                case "35":
+                    mark_Ukr = "167 балів";
+                    break;
+
+                case "36":
+                    mark_Ukr = "170 балів";
+                    break;
+
+                case "37":
+                    mark_Ukr = "172 бали";
+                    break;
+
+                case "38":
+                    mark_Ukr = "175 балів";
+                    break;
+
+                case "39":
+                    mark_Ukr = "177 балів";
+                    break;
+
+                case "40":
+                    mark_Ukr = "180 балів";
+                    break;
+
+                case "41":
+                    mark_Ukr = "183 бали";
+                    break;
+
+                case "42":
+                    mark_Ukr = "186 балів";
+                    break;
+
+                case "43":
+                    mark_Ukr = "191 бал";
+                    break;
+
+                case "44":
+                    mark_Ukr = "195 балів";
+                    break;
+
+                case "45":
+                    mark_Ukr = "200 балів";
+                    break;
+            }
+
+            string Math = textBox2.Text;
+            string mark_Math = Math;
+            switch(Math)
+            {
+                case "5":
+                    mark_Math = "100 балів";
+                    break;
+
+                case "6":
+                    mark_Math = "108 балів";
+                    break;
+
+                case "7":
+                    mark_Math = "115 балів";
+                    break;
+
+                case "8":
+                    mark_Math = "123 бали";
+                    break;
+
+                case "9":
+                    mark_Math = "131 бал";
+                    break;
+
+                case "10":
+                    mark_Math = "134 бали";
+                    break;
+
+                case "11":
+                    mark_Math = "137 балів";
+                    break;
+
+                case "12":
+                    mark_Math = "140 балів";
+                    break;
+
+                case "13":
+                    mark_Math = "143 бали";
+                    break;
+
+                case "14":
+                    mark_Math = "145 балів";
+                    break;
+
+                case "15":
+                    mark_Math = "147 балів";
+                    break;
+
+                case "16":
+                    mark_Math = "148 балів";
+                    break;
+
+                case "17":
+                    mark_Math = "149 балів";
+                    break;
+
+                case "18":
+                    mark_Math = "150 балів";
+                    break;
+
+                case "19":
+                    mark_Math = "151 бал";
+                    break;
+
+                case "20":
+                    mark_Math = "152 бали";
+                    break;
+
+                case "21":
+                    mark_Math = "155 балів";
+                    break;
+
+                case "22":
+                    mark_Math = "159 балів";
+                    break;
+
+                case "23":
+                    mark_Math = "163 бали";
+                    break;
+
+                case "24":
+                    mark_Math = "167 балів";
+                    break;
+
+                case "25":
+                    mark_Math = "170 балів";
+                    break;
+
+                case "26":
+                    mark_Math = "173 бали";
+                    break;
+
+                case "27":
+                    mark_Math = "176 балів";
+                    break;
+
+                case "28":
+                    mark_Math = "180 балів";
+                    break;
+
+                case "29":
+                    mark_Math = "184 бали";
+                    break;
+
+                case "30":
+                    mark_Math = "189 балів";
+                    break;
+
+                case "31":
+                    mark_Math = "194 бали";
+                    break;
+
+                case "32":
+                    mark_Math = "200 балів";
+                    break;
+            }
+
+            string Hystory = textBox3.Text;
+            string mark_Hystory = Hystory;
+            switch(Hystory)
+            {
+                case "8":
+                    mark_Hystory = "100 балів";
+                    break;
+
+                case "9":
+                    mark_Hystory = "105 балів";
+                    break;
+
+                case "10":
+                    mark_Hystory = "111 балів";
+                    break;
+
+                case "11":
+                    mark_Hystory = "116 балів";
+                    break;
+
+                case "12":
+                    mark_Hystory = "120 балів";
+                    break;
+
+                case "13":
+                    mark_Hystory = "124 бали";
+                    break;
+
+                case "14":
+                    mark_Hystory = "127 балів";
+                    break;
+
+                case "15":
+                    mark_Hystory = "130 балів";
+                    break;
+
+                case "16":
+                    mark_Hystory = "132 бали";
+                    break;
+
+                case "17":
+                    mark_Hystory = "134 бали";
+                    break;
+
+                case "18":
+                    mark_Hystory = "136 балів";
+                    break;
+
+                case "19":
+                    mark_Hystory = "138 балів";
+                    break;
+
+                case "20":
+                    mark_Hystory = "140 балів";
+                    break;
+
+                case "21":
+                    mark_Hystory = "141 бал";
+                    break;
+
+                case "22":
+                    mark_Hystory = "142 бали";
+                    break;
+
+                case "23":
+                    mark_Hystory = "143 бали";
+                    break;
+
+                case "24":
+                    mark_Hystory = "144 бали";
+                    break;
+
+                case "25":
+                    mark_Hystory = "145 балів";
+                    break;
+
+                case "26":
+                    mark_Hystory = "146 балів";
+                    break;
+
+                case "27":
+                    mark_Hystory = "147 балів";
+                    break;
+
+                case "28":
+                    mark_Hystory = "148 балів";
+                    break;
+
+                case "29":
+                    mark_Hystory = "149 балів";
+                    break;
+
+                case "30":
+                    mark_Hystory = "150 балів";
+                    break;
+
+                case "31":
+                    mark_Hystory = "151 бал";
+                    break;
+
+                case "32":
+                    mark_Hystory = "152 бали";
+                    break;
+
+                case "33":
+                    mark_Hystory = "154 бали";
+                    break;
+
+                case "34":
+                    mark_Hystory = "156 балів";
+                    break;
+
+                case "35":
+                    mark_Hystory = "158 балів";
+                    break;
+
+                case "36":
+                    mark_Hystory = "160 балів";
+                    break;
+
+                case "37":
+                    mark_Hystory = "163 бали";
+                    break;
+
+                case "38":
+                    mark_Hystory = "166 балів";
+                    break;
+
+                case "39":
+                    mark_Hystory = "168 балів";
+                    break;
+
+                case "40":
+                    mark_Hystory = "169 балів";
+                    break;
+
+                case "41":
+                    mark_Hystory = "170 балів";
+                    break;
+
+                case "42":
+                    mark_Hystory = "172 бали";
+                    break;
+
+                case "43":
+                    mark_Hystory = "173 бали";
+                    break;
+
+                case "44":
+                    mark_Hystory = "175 балів";
+                    break;
+
+                case "45":
+                    mark_Hystory = "177 балів";
+                    break;
+
+                case "46":
+                    mark_Hystory = "179 балів";
+                    break;
+
+                case "47":
+                    mark_Hystory = "181 бал";
+                    break;
+
+                case "48":
+                    mark_Hystory = "183 бали";
+                    break;
+
+                case "49":
+                    mark_Hystory = "185 балів";
+                    break;
+
+                case "50":
+                    mark_Hystory = "188 балів";
+                    break;
+
+                case "51":
+                    mark_Hystory = "191 бал";
+                    break;
+
+                case "52":
+                    mark_Hystory = "194 бали";
+                    break;
+
+                case "53":
+                    mark_Hystory = "197 балів";
+                    break;
+
+                case "54":
+                    mark_Hystory = "200 балів";
+                    break;
+            }
+
+            
+            MessageBox.Show("Результати:  " +
+                            "\nУкраїнська мова: " + mark_Ukr +
+                            "\nМатематика: " + mark_Math +
+                            "\nІсторія України: " + mark_Hystory);
+        }
+    }
+}
